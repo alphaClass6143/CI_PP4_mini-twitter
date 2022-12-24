99f5cb9db728f961c992875c7c9a3526db6f4efb
alphaClass6143 <alphaClass@web.de>
2022-12-24 02:54:20 +0100

    Added first basic models

--- home/models.py
+++ home/models.py
@@ -1,3 +1,62 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.db import models
+
+class User(BaseUserManager):
+    """
+    Custom user model manager where email is the unique identifiers
+    for authentication instead of usernames.
+    """
+    def create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        profile_picture = models.ImageField(
+            upload_to='profile_pictures', 
+            blank=True)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, password, **extra_fields)
+
+class FollowRelation(models.Model):
+    date_followed = models.DateTimeField()
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user")
+    user_followed = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user")
+
+
+class Post(models.Model):
+    content = models.TextField(
+        max_length=500
+    )
+    created_on = models.DateTimeField()
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user")
+
+class Comment(models.Model):
+    content = models.TextField(
+        max_length = 500
+    )
+    
+    created_on = models.DateTimeField()
+
+    post = models.ForeignKey(
+        Post, 
+        on_delete=models.CASCADE, 
+        related_name="comments"
+    )
+
+    user = models.ForeignKey(
+        User, 
+        on_delete=models.CASCADE, 
+        related_name="user"
+    )
--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.test import TestCase
+from .models import CustomUser
+
+class CustomUserTestCase(TestCase):
+    def setUp(self):
+        CustomUser.objects.create(email="user@example.com", password="password")
+        CustomUser.objects.create(email="other@example.com", password="password")
+
+    def test_users_have_emails(self):
+        user1 = CustomUser.objects.get(email="user@example.com")
+        user2 = CustomUser.objects.get(email="other@example.com")
+        self.assertEqual(user1.email, "user@example.com")
+        self.assertEqual(user2.email, "other@example.com")
+
+    def test_password_hashing(self):
+        user1 = CustomUser.objects.get(email="user@example.com")
+        user2 = CustomUser.objects.get(email="other@example.com")
+        self.assertNotEqual(user1.password, "password")
+        self.assertNotEqual(user2.password, "password")
+        self.assertTrue(user1.check_password("password"))
+        self.assertTrue(user2.check_password("password"))
+
