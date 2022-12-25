9b2fb7913fa8b5393beba30bc7ec22416742db52
alphaClass6143 <alphaClass@web.de>
2022-12-25 03:54:17 +0100

    Added basic index design

--- home/models.py
+++ home/models.py
@@ -67,8 +67,8 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     objects = CustomUserManager()
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username', 'password']
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email', 'password']
 
 
 
