bf4f35cec45834eb335b8db0e8b89fff1bf8d96f
alphaClass6143 <alphaClass@web.de>
2022-12-26 22:28:30 +0100

    Added search view

--- home/tests.py
+++ home/tests.py
@@ -3,23 +3,24 @@ from django.test import TestCase
 # Create your tests here.
 from django.test import TestCase
 from .models import CustomUser
+from django.urls import reverse
 
-class CustomUserTestCase(TestCase):
-    def setUp(self):
-        CustomUser.objects.create(email="user@example.com", password="password")
-        CustomUser.objects.create(email="other@example.com", password="password")
+class ViewTestCase(TestCase):
+    def test_home_view_with_offset(self):
+        # Create some test data
+        for i in range(20):
+            Post.objects.create(title=f'Post {i}', body='Test body')
 
-    def test_users_have_emails(self):
-        user1 = CustomUser.objects.get(email="user@example.com")
-        user2 = CustomUser.objects.get(email="other@example.com")
-        self.assertEqual(user1.email, "user@example.com")
-        self.assertEqual(user2.email, "other@example.com")
+        # Send a GET request to the home view with an offset of 10
+        response = self.client.get(reverse('home'), {'offset': 10})
+        self.assertEqual(response.status_code, 200)
 
-    def test_password_hashing(self):
-        user1 = CustomUser.objects.get(email="user@example.com")
-        user2 = CustomUser.objects.get(email="other@example.com")
-        self.assertNotEqual(user1.password, "password")
-        self.assertNotEqual(user2.password, "password")
-        self.assertTrue(user1.check_password("password"))
-        self.assertTrue(user2.check_password("password"))
+        # Check that the correct template is being used
+        self.assertTemplateUsed(response, 'home/index.html')
+
+        # Check that the correct context variables are being passed to the template
+        self.assertIn('post_list', response.context)
+        self.assertEqual(len(response.context['post_list']), 10)
+        self.assertEqual(response.context['post_list'][0].title, 'Post 9')
+        self.assertIn('form', response.context)
 
